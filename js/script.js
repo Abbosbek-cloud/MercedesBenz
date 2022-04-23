644378c1111a835bd9075082facd2af65a38be61
Abbosbek Sulaymonov <80127186+Abbosbek-cloud@users.noreply.github.com>
2022-04-23 12:14:17 +0500

    Updated

--- js/script.js
+++ js/script.js
@@ -1,19 +1,388 @@
-import accordion from "./modules/accordion";
-import classes from "./modules/classes";
-import data from "./modules/data";
-import form from "./modules/form";
-import loader from "./modules/loader";
-import modal from "./modules/modal";
-import slider from "./modules/slider";
-// import tabs from "./modules/tab";
-
 window.addEventListener("DOMContentLoaded", () => {
-  accordion();
-  classes();
-  data();
-  form();
-  loader();
-  modal();
-  slider();
-  // tabs();
+  const data = {
+    menu: [
+      {
+        img: "img/tabs/1.jpg",
+        altimg: "vegy",
+        title: "2021 Mercedes-Benz C-Class",
+        descr:
+          "The 2021 Mercedes-Benz C-Class finishes in the top half of our luxury small car rankings. It's powerful and upscale, but it has so-so...",
+        price: 245,
+      },
+      {
+        img: "img/tabs/2.jpg",
+        altimg: "vegy",
+        title: "2021 Mercedes-Benz C-Class'",
+        descr:
+          "The 2021 Mercedes-Benz C-Class finishes in the top half of our luxury small car rankings. It's powerful and upscale, but it has so-so...",
+        price: 230,
+      },
+      {
+        img: "img/tabs/3.jpg",
+        altimg: "vegy",
+        title: "2021 Mercedes-Benz C-Class'",
+        descr:
+          "The 2021 Mercedes-Benz C-Class finishes in the top half of our luxury small car rankings. It's powerful and upscale, but it has so-so...",
+        price: 245,
+      },
+    ],
+    requests: [
+      {
+        name: "await",
+        phone: "+998976785734",
+        id: 1,
+      },
+      {
+        name: "await",
+        phone: "+998976785734",
+        id: 2,
+      },
+      {
+        name: "abek01",
+        phone: "76876",
+        id: 3,
+      },
+      {
+        name: "abek01",
+        phone: "76876",
+        id: 4,
+      },
+      {
+        name: "abek01",
+        phone: "76876",
+        id: 5,
+      },
+    ],
+  };
+
+  //   Modal
+
+  const allModalBtn = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal");
+
+  allModalBtn.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimer);
+  }
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal || e.target.getAttribute("data-close") === "") {
+      closeModal();
+    }
+  });
+
+  const modalTimer = setTimeout(openModal, 50000);
+
+  function showMyModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showMyModalByScroll);
+    }
+  }
+  window.addEventListener("scroll", showMyModalByScroll);
+
+  // TABS
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabContent = document.querySelectorAll(".tabcontent"),
+    headerParents = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabContent.forEach((item) => {
+      item.style.display = "none";
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  headerParents.addEventListener("click", (event) => {
+    console.log(event);
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  //   slider
+
+  const slides = document.querySelectorAll(".offer__slide"),
+    slider = document.querySelector(".offer__slider"), // get a parent div of all slides
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next"),
+    current = document.querySelector("#current"),
+    total = document.querySelector("#total"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    width = window.getComputedStyle(slidesWrapper).width,
+    slidesField = document.querySelector(".offer__slider_inner");
+
+  let slideIndex = 1,
+    offset = 0;
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+  } else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+  }
+
+  slidesField.style.width = 100 * slides.length + "%";
+
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  slider.style.position = "relative";
+  let indicator = document.createElement("ol"),
+    dots = [];
+
+  indicator.style.cssText = `
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 15;
+      display: flex;
+      justify-content: center;
+      margin-right: 15%;
+      margin-left: 15%;
+      list-style: none
+      `;
+
+  slider.append(indicator);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin: 0 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transform: opacity .6s ease;
+      `;
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicator.append(dot);
+    dots.push(dot);
+  }
+
+  next.addEventListener("click", () => {
+    if (offset == +width.replace(/\D/g, "") * (slides.length - 1)) {
+      // 650
+      offset = 0;
+    } else {
+      offset += +width.replace(/\D/g, "");
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+    dots.forEach((dot) => (dot.style.opacity = ".5"));
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  prev.addEventListener("click", () => {
+    if (offset == 0) {
+      offset = +width.replace(/\D/g, "") * (slides.length - 1);
+    } else {
+      offset -= +width.replace(/\D/g, "");
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  });
+
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+
+      slideIndex = slideTo;
+      offset = +width.replace(/\D/g, "") * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach((dot) => (dot.style.opacity = "0.5"));
+      dots[slideIndex - 1].style.opacity = 1;
+    });
+  });
+  //   classes
+
+  class CarCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transferToUsd = 10.8;
+      this.transferSoumToDollar();
+    }
+
+    transferSoumToDollar() {
+      this.price = this.price * this.transferToUsd;
+    }
+
+    render() {
+      const div = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        div.classList.add(this.classes);
+      } else {
+        this.classes.forEach((className) => div.classList.add(className));
+      }
+      div.innerHTML = `
+              <div class="menu__item">
+                  <img src=${this.src} alt=${this.alt} />
+                  <h3 class="menu__item-subtitle">${this.title}</h3>  
+                  <div class="menu__item-descr">
+                      ${this.descr}
+                  </div>
+                  <div class="menu__item-divider"></div>
+                  <div class="menu__item-price">
+                      <div class="menu__item-cost">Price: </div>
+                      <div class="menu__item-total">
+                          <span>${this.price}</span>
+                      </div>
+                  </div>
+              </div>
+            `;
+      this.parent.append(div);
+    }
+  }
+
+  data.menu.forEach(({ img, altimg, descr, price, title }) => {
+    new CarCard(img, altimg, title, descr, price, ".menu .container").render();
+  });
+
+  //   dates
+  const deadline = "2022-08-11";
+
+  function getTime(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((total / 1000) % 60),
+      minutes = Math.floor((total / 1000 / 60) % 60),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    return {
+      total: total,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const time = getTime(endtime);
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
+
+  //   accordion
+  const accordion = document.querySelectorAll(".accordion");
+
+  accordion.forEach((acc) => {
+    acc.addEventListener("click", () => {
+      acc.classList.toggle("active");
+      const panel = acc.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  });
 });
